ffa7adfe7ef774010ce4a009ec1e39cd267238af
HappyTheExceed <jabbysobrevinas@yahoo.com>
2019-05-03 10:18:20 +0800

    Update discord_bot.js

--- discord_bot.js
+++ discord_bot.js
@@ -284,7 +284,7 @@ bot.on("ready", function () {
 	console.log("type "+Config.commandPrefix+"help in Discord for a commands list.");
 	bot.user.setPresence({
 		game: {
-			name: 'Sewayaki Kitsune no Senko-san'
+			name: '!k | Kirby’s Personal Bot',
             type: "STREAMING",
             url: "https://www.twitch.tv/TwitchPresents"
 		}
