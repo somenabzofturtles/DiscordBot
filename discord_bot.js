2414e16940a421f415c3c05f14d41160f402439e
HappyTheExceed <jabbysobrevinas@yahoo.com>
2019-05-03 08:39:14 +0800

    Update discord_bot.js

--- discord_bot.js
+++ discord_bot.js
@@ -285,7 +285,12 @@ bot.on("ready", function () {
 	console.log("type "+Config.commandPrefix+"help in Discord for a commands list.");
 	bot.user.setPresence({
 		game: {
-			name: 'Sewayaki Kitsune no Senko-san' , type: 'WATCHING'
+			let activityTypes = ['PLAYING','STREAMING','LISTENING','WATCHING']
+let randomType = activityTypes[Math.floor((Math.random()*activityTypes.length))]
+
+setInterval(async ()=>{
+    await client.user.setActivity('Sewayaki Kitsune no Senko San', { type: STREAMING })
+},15000)
 		}
 	}); 
 });
